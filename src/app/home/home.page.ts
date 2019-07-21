306d05eb83e4b0fc71d7e6274723a52d6d6454b4
Zhaoxian Hu <huzhx@uci.edu>
2019-07-20 19:56:37 -0700

    add options

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,10 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  options: string[];
 
-  constructor() {}
-
+  constructor() {
+    this.options = [
+      'Good parent',
+      'Disciplined',
+      'Good community member',
+      'Staying close to extended family',
+      'Strong',
+      'On top of things',
+      'Respected at home',
+      'Considerate',
+      'Responsible',
+      'In control',
+      'Youthful',
+      'Popular',
+      'Independent',
+      'Energetic',
+      'Competent',
+      'Good spouse/partner',
+      'Not hypocritical',
+      'Spiritual',
+      'Doing God’s will'
+    ];
+  }
 }
