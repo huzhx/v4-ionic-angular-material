d468a8fae7a3b92617c472c79a1f2db6fea314c9
Zhaoxian Hu <huzhx@uci.edu>
2019-07-22 13:45:19 -0700

    create a dummy radio button group to replicate the issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,10 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  options: string[];
 
-  constructor() {}
-
+  constructor() {
+    this.options = [
+      'Reduce ...',
+      'Stay ...',
+      'Try...',
+      'Write ...',
+      'Find ...',
+      'Talk ...',
+      'Make ...',
+      'Stop ...',
+      'Choose ...',
+      'Replace ...',
+      'Read ...',
+      'Get ...',
+      'Ask ...',
+      'Track ...',
+      'Become ...',
+      'Act ...',
+      'Other'
+    ];
+  }
 }
